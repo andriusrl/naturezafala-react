a5b55e29022a4ea0f96541fbcc3ed2407eb298e4
Andrius <andrius.rochalazarino@gmail.com>
2023-10-17 16:01:02 -0400

    feat: jest configuration

---
+++ jest.config.ts
@@ -0,0 +1,36 @@
+const assetsKey = '\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|css)$'
+
+const config = {
+
+  clearMocks: true,
+
+  collectCoverage: true,
+
+  coverageDirectory: 'coverage',
+
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    'config/tests',
+  ],
+
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1',
+    '\\.svg$': '<rootDir>/src/config/tests/mocks/svg.ts',
+    [assetsKey]: 'ts-jest',
+  },
+
+ 
+  roots: ['<rootDir>'],
+
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+
+  testEnvironment: 'jsdom',
+
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+    [assetsKey]: 'ts-jest',
+    '\\.svg$': 'ts-jest',
+  },
+}
+
+export default config
--- src/shared/App/index.spec.tsx
+++ src/shared/App/index.spec.tsx
@@ -1,9 +1,23 @@
-describe("Jest", () => {
-  it("should work", () => {
-    expect(1).toBe(1);
-  });
+import React from "react"
+import "@testing-library/jest-dom";
+import { render } from "@testing-library/react";
+import Home from "../../routes/Home";
 
-  it("should work 2", () => {
-    expect(2).toBe(2);
-  });
+test("demo", () => {
+  expect(true).toBe(true);
 });
+
+test("Renders the main page", () => {
+  render(<Home />);
+  expect(true).toBeTruthy();
+});
+
+// describe("Jest", () => {
+//   it("should work", () => {
+//     expect(1).toBe(1);
+//   });
+
+//   it("should work 2", () => {
+//     expect(2).toBe(2);
+//   });
+// });
