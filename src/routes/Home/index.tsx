5d26be032c1d98c6f746f25738b4cbd27f54dde8
Andrius <andrius.rochalazarino@gmail.com>
2023-10-21 12:08:27 -0400

    feat: map openstreet

--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -1,7 +1,48 @@
 import React from 'react'
+import { MapContainer, Marker, Popup, TileLayer} from 'react-leaflet'
+import 'leaflet/dist/leaflet.css'
+import "leaflet/dist/leaflet.css"
+import "leaflet/dist/images/marker-shadow.png"
 
-function Home() {
-  return <div className="bg-green-600">Natureza Fala</div>;
+
+export default function Mapping() {
+
+  const [mapState, setMapState] = React.useState(
+    {
+      lat: 41.257017,
+      lng: 29.077524,
+      zoom: 13,
+    }
+  )
+
+  return (
+    <div>
+
+      <div id="map">
+
+        <MapContainer
+          center={[51.505, -0.09]}
+          zoom={13}
+          scrollWheelZoom={false}
+          style={{ height: '100vh', width: '100wh' }}
+        >
+
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+
+          <Marker position={[51.505, -0.09]}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
+
+
+      </div>
+
+
+    </div>
+  )
 }
-
-export default Home;
