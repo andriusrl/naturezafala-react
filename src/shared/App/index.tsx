a16cde810cce40886fde6ca6ec3833f2fa97159b
Andrius <andrius.rochalazarino@gmail.com>
2023-10-25 20:37:43 -0400

    feat: footer customization

--- src/shared/App/index.tsx
+++ src/shared/App/index.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 function App() {
   return (
     <>
       <Navbar />
       <Outlet />
-      <h1>Footer</h1>
+      <Footer />
     </>
   );
 }
---
+++ src/shared/components/Footer/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="flex justify-center text-3xl font-extrabold">
+        <span className="text-[#0A5B0D]">Natureza</span>
+        <span className="text-[#944B0A]">Fala</span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
