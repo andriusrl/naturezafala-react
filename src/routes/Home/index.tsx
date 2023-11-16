b346b8a3e99200f9dd27ab286a6414f2a0d0e45d
Andrius <andrius.rochalazarino@gmail.com>
2023-11-16 16:23:43 -0400

    feat: mark png button

--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import Map from "../../shared/components/Map";
+import MarkPng from "../../assets/mark.png";
 
 export default function Home() {
   return (
     <div>
       <Map />
+      <div>
+        <img
+          className="w-[60px] fixed bottom-6 right-5 z-40 cursor-pointer animate-pulse"
+          src={MarkPng}
+          // onClick={ }
+        />
+      </div>
     </div>
   );
 }
