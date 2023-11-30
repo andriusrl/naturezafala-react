03fe4c83e9e07f004f244c91365635cbb368a0c3
Andrius <andrius.rochalazarino@gmail.com>
2023-11-30 09:01:15 -0400

    feat: message if email or cpf has already been registered

--- src/routes/Register/index.tsx
+++ src/routes/Register/index.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
+import api from "../../config/axios/api";
+import { useNavigate } from "react-router-dom";
 
 export default function Register() {
+  const navigate = useNavigate();
 
   type loginType = {
     name: string;
@@ -11,23 +14,41 @@ export default function Register() {
     fone: number;
     cpf: string;
     confirmPassword: string;
-  }
+  };
 
   const {
     register,
     handleSubmit,
-    watch,
     formState: { errors },
-  } = useForm<loginType>()
+  } = useForm<loginType>();
 
+  const onSubmit: SubmitHandler<loginType> = async (data) => {
+    if (data.password !== data.confirmPassword) {
+      return alert("Senhas não conferem");
+    }
 
-  const onSubmit: SubmitHandler<loginType> = (data) => {
+    const userBody = {
+      name: data.name,
+      email: data.email,
+      password: data.password,
+      birthDate: data.birthDate,
+      fone: data.fone,
+      cpf: data.cpf,
+    };
 
+    try {
+      await api.post("/user", userBody);
 
-    console.log(data)
-    console.log('registrado')
+      alert("Usuário cadastrado com sucesso");
 
-  }
+      navigate("/");
+    } catch (error) {
+      if (error.response.data.message === "user_un_cpf")
+        return alert("CPF já cadastrado");
+      if (error.response.data.message === "user_un_email")
+        return alert("E-mail já cadastrado");
+    }
+  };
 
   // console.log(watch("email"))
 
@@ -38,42 +59,108 @@ export default function Register() {
           <h2 className="w-fit mx-auto text-3xl">Faça seu registro</h2>
 
           <div className="mt-2">
-            <input placeholder="Nome completo" className="text-2xl pl-2 border border-slate-400 rounded-md" {...register("name", { required: true })} />
-            {errors.name && <div className="w-fit mt-1 text-red-600">É necessário colocar o nome</div>}
+            <input
+              placeholder="Nome completo"
+              className="text-2xl pl-2 border border-slate-400 rounded-md"
+              {...register("name", { required: true })}
+            />
+            {errors.name && (
+              <div className="w-fit mt-1 text-red-600">
+                É necessário colocar o nome
+              </div>
+            )}
           </div>
 
           <div className="mt-2">
-            <input placeholder="E-mail" className="text-2xl pl-2 border border-slate-400 rounded-md" {...register("email", { required: true })} />
-            {errors.email && <div className="w-fit mt-1 text-red-600">É necessário colocar o email</div>}
+            <input
+              placeholder="E-mail"
+              className="text-2xl pl-2 border border-slate-400 rounded-md"
+              {...register("email", { required: true })}
+            />
+            {errors.email && (
+              <div className="w-fit mt-1 text-red-600">
+                É necessário colocar o email
+              </div>
+            )}
           </div>
 
           <div className="mt-2">
-            <input type="date" placeholder="Data de nascimento" className="text-2xl pl-2 border border-slate-400 rounded-md" {...register("birthDate", { required: true })} />
-            {errors.birthDate && <div className="w-fit mt-1 text-red-600">É necessário colocar a data de nascimento</div>}
+            <input
+              type="date"
+              placeholder="Data de nascimento"
+              className="w-full text-2xl pl-2 border border-slate-400 rounded-md"
+              {...register("birthDate", { required: true })}
+            />
+            {errors.birthDate && (
+              <div className="w-fit mt-1 text-red-600">
+                É necessário colocar a data de nascimento
+              </div>
+            )}
           </div>
 
           <div className="mt-2">
-            <input maxLength={12} placeholder="Telefone" className="text-2xl pl-2 border border-slate-400 rounded-md" {...register("fone", { required: true })} />
-            {errors.fone && <div className="w-fit mt-1 text-red-600">É necessário colocar o telefone</div>}
+            <input
+              maxLength={12}
+              placeholder="Telefone"
+              className="text-2xl pl-2 border border-slate-400 rounded-md"
+              {...register("fone", { required: true })}
+            />
+            {errors.fone && (
+              <div className="w-fit mt-1 text-red-600">
+                É necessário colocar o telefone
+              </div>
+            )}
           </div>
 
           <div className="mt-2">
-            <input maxLength={11} placeholder="CPF" className="text-2xl pl-2 border border-slate-400 rounded-md" {...register("cpf", { required: true })} />
-            {errors.cpf && <div className="w-fit mt-1 text-red-600">É necessário colocar o cpf</div>}
+            <input
+              maxLength={11}
+              placeholder="CPF"
+              className="text-2xl pl-2 border border-slate-400 rounded-md"
+              {...register("cpf", { required: true })}
+            />
+            {errors.cpf && (
+              <div className="w-fit mt-1 text-red-600">
+                É necessário colocar o cpf
+              </div>
+            )}
           </div>
 
           <div className="mt-2">
-            <input maxLength={30} placeholder="Senha" className="text-2xl pl-2 border border-slate-400 rounded-md" {...register("password", { required: true })} />
-            {errors.password && <div className="w-fit mt-1 text-red-600">É necessário colocar a senha</div>}
+            <input
+              maxLength={30}
+              placeholder="Senha"
+              className="text-2xl pl-2 border border-slate-400 rounded-md"
+              {...register("password", { required: true })}
+            />
+            {errors.password && (
+              <div className="w-fit mt-1 text-red-600">
+                É necessário colocar a senha
+              </div>
+            )}
           </div>
 
           <div className="mt-2">
-            <input maxLength={30} placeholder="Senha" className="text-2xl pl-2 border border-slate-400 rounded-md" {...register("confirmPassword", { required: true })} />
-            {errors.confirmPassword && <div className="w-fit mt-1 text-red-600">É necessário confirmar a senha</div>}
+            <input
+              maxLength={30}
+              placeholder="Confirme a senha"
+              className="text-2xl pl-2 border border-slate-400 rounded-md"
+              {...register("confirmPassword", { required: true })}
+            />
+            {errors.confirmPassword && (
+              <div className="w-fit mt-1 text-red-600">
+                É necessário confirmar a senha
+              </div>
+            )}
           </div>
         </div>
         <div className="flex justify-center mt-2">
-          <button type="submit" className="animate-pulse bg-slate-400 rounded-lg p-2 font-extrabold text-xl">Entrar</button>
+          <button
+            type="submit"
+            className="animate-pulse bg-slate-400 rounded-lg p-2 font-extrabold text-xl"
+          >
+            Cadastrar
+          </button>
         </div>
       </div>
     </form>
